f3e62b87ff3613b1f7c2cacd9d259ca4efc47aae
Anshu Ayan <kakarotp@proton.me>
2023-04-03 01:49:00 +0530

    Update __init__.py

--- Hentai/__init__.py
+++ Hentai/__init__.py
@@ -13,7 +13,7 @@ LOG.setLevel(level=logging.INFO)
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 API_ID = environ.get('API_ID', 15849735)
 API_HASH = environ.get('API_HASH', 'b8105dc4c17419dfd4165ecf1d0bc100')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6016198379:AAEmkOI6lMgt0n2zPDorTGwYb88do')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6016198379:AAEmkOI6lMgt0n2zPDorTGwY1yk3Ijb88do')
 DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://cloud:cloud69@cluster0.bieyjxe.mongodb.net/?retryWrites=true&w=majority')
 OWNER_ID = 669641125
 DEV_USERS = [1695508822, 5110399944, 720518864]
